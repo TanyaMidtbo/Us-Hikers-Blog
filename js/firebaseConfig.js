0d20f4d24587c6b805bdbaa4919999fd6ada269d
Tetiana Koliesnik <144105771+TetianaKoliesnik@users.noreply.github.com>
2024-06-11 18:10:47 +0200

    add firebase

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCHGEKbZRJsWUU63FC1U0vB0sFe63kHR7A",
+	authDomain: "hikers-auth.firebaseapp.com",
+	projectId: "hikers-auth",
+	storageBucket: "hikers-auth.appspot.com",
+	messagingSenderId: "842020339814",
+	appId: "1:842020339814:web:4657b0d45e2c944d882da7"
+  };
+
+  export default firebaseConfig;
